308feff0cbc564a9ac25e76159c7da49d63fd060
gustavo-h-k-oliveira <gustavo.oliveira204@fatec.sp.gov.br>
2026-03-15 18:06:20 -0300

    Adicionando threading no main para separar o consumidor MQTT da aplicação principal.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,11 @@
+import threading
 from mqtt.consumer import start_mqtt_consumer
 
+def main():
+    mqtt_thread = threading.Thread(target=start_mqtt_consumer)
+    mqtt_thread.start()
+
+    print("Backend iniciado")
+
 if __name__ == "__main__":
-    start_mqtt_consumer()
+    main()
